4e8648e774dfde3035f70476122d9d6b23e4b13b
Aneta Drahoňovská <drahoane@fel.cvut.cz>
2022-05-10 09:49:34 +0000

    Replace MouseInput.java

--- projekt/main/java/MouseInput.java
+++ projekt/main/java/MouseInput.java
@@ -2,12 +2,17 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class MouseInput extends MouseAdapter {
-    private Handler handler;
-    private Camera camera;
+    private final Handler handler;
+    private final Camera camera;
+    private final Game game;
+    private final Sheet sh;
 
-    public MouseInput(Handler handler, Camera camera) {
+
+    public MouseInput(Handler handler, Camera camera, Game game, Sheet sh) {
         this.handler = handler;
         this.camera = camera;
+        this.game = game;
+        this.sh = sh;
     }
 
     /**
@@ -20,12 +25,36 @@ public class MouseInput extends MouseAdapter {
         int mx = (int) (e.getX() + camera.getX());
         int my = (int) (e.getY() + camera.getY());
 
-        for(int i = 0; i < handler.object.size(); i++) {
-            GameObject tempObject = handler.object.get(i);
+        for(int i = 0; i < handler.objects.size(); i++) {
+            GameObject tempObject = handler.objects.get(i);
 
-            if(tempObject.getId() == ID.Player) {
-                handler.addObject(new Spell(tempObject.getX() +16, tempObject.getY() +24, ID.Spell, handler, mx, my));
+            if(tempObject.getId() == ID.Player && game.ply.mana >= 1) {     //cannot shoot the spell without mana
+                handler.addObject(new Spell(tempObject.getX() +16, tempObject.getY() +24, ID.Spell, handler, sh, mx, my));    //+16 and +24 -> spell goes from the middle of the player object
+                game.ply.mana--;
+            }
+        }
+
+
+        int ax = e.getXOnScreen();
+        int ay = e.getYOnScreen();
+
+        if(game.gameState == Game.STATE.Game) {
+            if (mouseOver(ax, ay, 675, 162, 875, 182)) {        //serialize
+                System.out.println("Saving....");
+                game.gameState = Game.STATE.Save;
+            }
+        }
+
+        if(game.gameState == Game.STATE.Defeat || game.gameState == Game.STATE.Victory) {
+            if (mouseOver(ax, ay, 665, 455, 865, 520)) {        //exit
+                System.out.println("Terminating....");
+                System.exit(0);
             }
         }
     }
+
+    private boolean mouseOver(int ax, int ay, int x, int y, int width, int height) {
+        return ax > x && ax < width && ay > y && ay < height;
+    }
+
 }
