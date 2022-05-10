89aedd1bfbf69f265297478b7cc3614d35520104
Aneta Drahoňovská <drahoane@fel.cvut.cz>
2022-05-10 09:49:20 +0000

    Replace KeyInput.java

--- projekt/main/java/KeyInput.java
+++ projekt/main/java/KeyInput.java
@@ -17,8 +17,8 @@ public class KeyInput extends KeyAdapter {
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
 
-        for(int i = 0; i < handler.object.size(); i++) {       //loop through all game objects
-            GameObject tempObject = handler.object.get(i);
+        for(int i = 0; i < handler.objects.size(); i++) {       //loop through all game objects
+            GameObject tempObject = handler.objects.get(i);
 
             if(tempObject.getId() == ID.Player) {      //if WASD is pressed and the object is a player, move in certain direction
                 if(key == KeyEvent.VK_W)  handler.setUp(true);
@@ -37,8 +37,8 @@ public class KeyInput extends KeyAdapter {
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
 
-        for(int i = 0; i < handler.object.size(); i++) {       //loop through all game objects
-            GameObject tempObject = handler.object.get(i);
+        for(int i = 0; i < handler.objects.size(); i++) {       //loop through all game objects
+            GameObject tempObject = handler.objects.get(i);
 
             if(tempObject.getId() == ID.Player) {      //if WASD is released and the object is a player, stop moving in certain direction
                 if(key == KeyEvent.VK_W)  handler.setUp(false);
