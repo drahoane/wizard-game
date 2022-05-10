e42093c75cf7f36962db06064ac4574f37b464c0
Aneta Drahoňovská <drahoane@fel.cvut.cz>
2022-05-10 09:44:10 +0000

    Replace Block.java

--- projekt/main/java/Block.java
+++ projekt/main/java/Block.java
@@ -1,21 +1,22 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Block extends GameObject{
 
+
     public Block(int x, int y, ID id) {
         super(x, y, id);
+
     }
+
+
 
     @Override
     public void tick() {
 
     }
 
-    @Override
-    public void render(Graphics g) {
-        g.setColor(Color.BLUE);
-        g.fillRect(x, y, 32, 32);
-    }
+
 
     @Override
     public Rectangle getBounds() {
