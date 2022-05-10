8a8fef06c8baf841f55620c1daa1a2193eb23762
Aneta Drahoňovská <drahoane@fel.cvut.cz>
2022-05-10 09:46:30 +0000

    Replace Block.java

--- projekt/main/java/Block.java
+++ projekt/main/java/Block.java
@@ -1,11 +1,14 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class Block extends GameObject{
+public class Block extends GameObject  {
 
+    //transient private final BufferedImage blockImg;
 
-    public Block(int x, int y, ID id) {
-        super(x, y, id);
+    public Block(int x, int y, ID id, Sheet sh) {
+        super(x, y, id, sh);
+        sheetX = 5; sheetY = 2; sheetSizeX = 32; sheetSizeY = 32;
+        //blockImg = sh.grab(5, 2, 32, 32);
 
     }
 
@@ -16,7 +19,10 @@ public class Block extends GameObject{
 
     }
 
-
+    /*@Override
+    public void render(Graphics g) {
+        g.drawImage(blockImg, x, y, null);
+    }*/
 
     @Override
     public Rectangle getBounds() {
